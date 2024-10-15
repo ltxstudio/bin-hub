3b742ee0c2dfb8f17c2d54901782b0f71b7b4f91
ltxstudio <ltxstudio.info@gmail.com>
2024-10-15 07:38:14 +0600

    Update Post.jsx

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
-const Post = ({ post }) => {
+interface PostProps {
+  post: {
+    title: string;
+    content: string;
+  };
+}
+
+const Post: React.FC<PostProps> = ({ post }) => {
   return (
     <div className="p-4 border rounded my-2">
       <h3 className="text-lg font-bold">{post.title}</h3>
